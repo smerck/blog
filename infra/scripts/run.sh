b7192e37fcc1ce4430e71a8903dda557100cd656
Stephan.Mercatoris <smmercatoris@gmail.com>
2022-04-06 13:56:09 -0400

    fixing run.sh script

--- infra/scripts/run.sh
+++ infra/scripts/run.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+CONFIG=""
+
+if [ -z "uname -a|grep 'Darwin'" ]; then
+  CONFIG="config.dev.toml"
+else 
+  CONFIG="config.toml"
+fi
+
+
 hugo server \
-  --bind=0.0.0.0
-  --hostname=http://blog.smerc.io
-  --disableFastRender
-  --config=config.toml
+  --bind=0.0.0.0 \
+  --hostname=http://blog.smerc.io \
+  --disableFastRender \
+  --config=$CONFIG \
