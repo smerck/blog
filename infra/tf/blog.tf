0c479405c4aee2a859bc58c45262091c61a6a2de
Smerc <43017340+smerck@users.noreply.github.com>
2022-04-06 20:51:16 -0400

    creating dev branch for profit and fun (#7)
    * chore: pre-commit
    
    * chore: fixing cors
    
    * chore: fixing repo in terraform :cry:

--- infra/tf/blog.tf
+++ infra/tf/blog.tf
@@ -2,15 +2,6 @@ module "blog_prod" {
   source = "./modules/digital_ocean/app"
 
   region = "nyc1"
-  name = "blog"
-  branch = "main" 
-}
-
-
-module "blog_test" {
-  source = "./modules/digital_ocean/app"
-
-  region = "nyc1"
-  name = "blog-test"
-  branch = "dev" 
+  name = "smerc-blog"
+  branch = "main"
 }
